81dbbef0d1d79ae26f1e7c14d8c4e9017a47ff55
Yosef Andrés Rivera Pinales <151891559+Andres250602@users.noreply.github.com>
2024-04-21 11:34:02 -0600

    Add files via upload

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import tarea
+
+class tareaAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
+# Register your models here.
+admin.site.register(tarea, tareaAdmin)
---
+++ myapp/views.py
@@ -0,0 +1,118 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from .forms import TaskForm
+from .models import tarea
+from django.utils import timezone
+
+
+def signup(request):
+    if request.method== 'GET':
+            return render(request, 'signup.html', {
+    'form': UserCreationForm
+    })
+        
+        
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return  render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'usuario ya existe'
+                })  
+            
+            
+
+        
+        return  render(request, 'signup.html', {
+                'form': UserCreationForm,
+                'error': 'contraseña no coincide'
+                })
+                
+
+    
+    return render(request, 'signup.html', {
+    'form': UserCreationForm
+    })
+def home(request):
+    return render (request, 'home.html')
+def tasks(request):
+    tasks = tarea.objects.filter(user=request.user, datecompleted__isnull=True )
+    return render(request, 'tasks.html', {'tasks': tasks})
+def signout(request):
+    logout(request)
+    return redirect ('home')
+def signin(request):
+    if request.method == 'GET':
+        return render (request, 'signin.html',{
+            'form': AuthenticationForm
+        }) 
+    else:   
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario y Contraseña incorrectos'
+            })
+        else:
+            login (request, user)
+            return redirect ('tasks')    
+def create_task(request):
+    if request.method == 'GET':
+        return render (request, 'create_task.html', {
+            'form':TaskForm
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect ('tasks')
+        except ValueError:
+            return render (request, 'create_task.html', {
+                'form':TaskForm,
+                'error':'Por favor de ingresar datos validos'
+                })
+def task_details(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(tarea, pk=task_id, user=request.user)
+        form = TaskForm(instance=tarea)
+        return render(request, 'task_details.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(tarea, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect ('tasks')
+        except ValueError:
+            return render(request, 'task_details.html', {'task': task, 'form': form, 'error': "Error al actualizar tarea"})
+def complete_task (request, task_id):
+    task = get_object_or_404(tarea, pk=task_id, user=request.user ) 
+    if request.method =='POST' :
+        task.datecompleted = timezone.now()
+        task.save()   
+        return redirect ('tasks')       
+def delete_task (request, task_id):
+    task = get_object_or_404(tarea, pk=task_id, user=request.user ) 
+    if request.method =='POST' :
+        task.datecompleted = timezone.now()
+        task.delete()   
+        return redirect ('tasks')       
+def tasks_completed(request):
+    tasks = tarea.objects.filter(user=request.user, datecompleted__isnull=False ).order_by 
+    ('-datecompleted')
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+
+    
+    
+    
+    
