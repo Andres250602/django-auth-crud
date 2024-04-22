e6330ad783f3429b061e03aece763b81b038a7fe
Yosef Andrés Rivera Pinales <151891559+Andres250602@users.noreply.github.com>
2024-04-22 11:04:18 -0600

    Delete mysite directory

--- mysite/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import tarea
-
-class tareaAdmin(admin.ModelAdmin):
-    readonly_fields = ("created", )
-# Register your models here.
-admin.site.register(tarea, tareaAdmin)
