7093ef67817a15d6fb913ab45d46ce289ecddc60
Yosef Andrés Rivera Pinales <151891559+Andres250602@users.noreply.github.com>
2024-04-22 10:30:36 -0600

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import tarea
-
-class TaskForm(ModelForm):
-    class Meta:
-        model = tarea  # Aquí estaba el error, Model debería ser model
-        fields = ['title', 'description', 'important']
